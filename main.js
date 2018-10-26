eeceb3322fcbad4416a845d90f77264d0c04f476
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-10-26 12:34:42 -0400

    Update orange color of logo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 let tl = new TimelineLite();
-tl.add([TweenLite.to('#logo', 5, {opacity: 1, ease: Linear.easeNone})])
+tl.add([TweenLite.to('#logo', 3.5, {opacity: 1, ease: Linear.easeNone})])
 // tl.add([TweenLite.to("#hello", .2, {opacity:1, ease:Linear.easeNone}),TweenLite.to("#l", .2, {opacity:1, ease:Linear.easeNone}))]);
 
 window.onload = function(){
