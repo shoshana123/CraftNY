4f71c037e26363cf2d307869991b0acc024186bf
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-10-26 12:25:46 -0400

    Update to include fade in of first logo

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let tl = new TimelineLite();
+tl.add([TweenLite.to('#logo', 5, {opacity: 1, ease: Linear.easeNone})])
+// tl.add([TweenLite.to("#hello", .2, {opacity:1, ease:Linear.easeNone}),TweenLite.to("#l", .2, {opacity:1, ease:Linear.easeNone}))]);
+
+window.onload = function(){
+  tl.play();
+};
